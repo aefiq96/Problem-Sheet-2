17f165c7a3a572439e5ab1be7602ad52b9f2d464
NUZHAFIQ NOOR AZMAN - Student <G00324934@gmit.ie>
2017-10-26 20:59:46 +0100

    Q4

--- GuessingGame.go
+++ GuessingGame.go
@@ -4,6 +4,7 @@ package main
 import (
 //	"fmt"	
 	"net/http"
+//	"html/template"
 )
 
 func Guess(w http.ResponseWriter, r *http.Request) {
@@ -12,8 +13,15 @@ func Guess(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w,r,"index.html")
 }
 
+func GuessRoute(w http.ResponseWriter, r *http.Request) {
+
+	//fmt.Fprint(w, "<h1>Guessing game</h1>")
+	http.ServeFile(w,r,"guess.html")
+}
+
 func main() {
 	//localhost:8080
 	http.HandleFunc("/", Guess)	
+	http.HandleFunc("/guess", GuessRoute)
 	http.ListenAndServe(":8080", nil)
 }

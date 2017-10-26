db9eb02d3b25c34f19db75bbb617909843fa31a2
NUZHAFIQ NOOR AZMAN - Student <G00324934@gmit.ie>
2017-10-26 21:07:53 +0100

    Q4part2

--- GuessingGame.go
+++ GuessingGame.go
@@ -1,12 +1,15 @@
 //G00324934 - Nuzhafiq Iqbal Noor Azman
+//https://gobyexample.com/structs
 package main
 
 import (
 //	"fmt"	
 	"net/http"
-//	"html/template"
+	"html/template"
 )
-
+type message struct{
+	Message string
+}
 func Guess(w http.ResponseWriter, r *http.Request) {
 
 	//fmt.Fprint(w, "<h1>Guessing game</h1>")
@@ -16,7 +19,14 @@ func Guess(w http.ResponseWriter, r *http.Request) {
 func GuessRoute(w http.ResponseWriter, r *http.Request) {
 
 	//fmt.Fprint(w, "<h1>Guessing game</h1>")
-	http.ServeFile(w,r,"guess.html")
+	//create a string 
+	str := "Guess a number between 1 and 20"
+	//pass it to a struct 
+	m := &message{Message: str}
+	//parse a file
+	tem, _ := template.ParseFiles("guess.tmpl")
+	tem.Execute(w,m)
+
 }
 
 func main() {
